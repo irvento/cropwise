60215449ad8ba19bb6ff022fbc97a0ff8da34e2c
Irven Abarquez <irvenabarquez@gmail.com>
2025-04-28 15:27:21 +0800

    added container for the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,14 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                
+
+<div class="container">
+    <div class="row">
+    </div>
+
+
+</div>
+
             </div>
         </div>
     </div>
