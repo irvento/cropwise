e82b2f7c59436ef8ae91d125d5186117519840d6
Irven Abarquez <irvenabarquez@gmail.com>
2025-04-28 15:31:36 +0800

    PS Disclaimer this is an initial dashboard for reference, this will be further fix in development

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-
+ <!-- Disclaimer this is an initial dashboard for reference, this will be further fix in development -->
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
