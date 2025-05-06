947b57832371d5f0f7493253a313226d458e8d4b
irvento <irvenabarquez@gmail.com>
2025-05-06 12:18:22 +0800

    edited tailwind config so it can recognized specific colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,27 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
+    safelist: [
+        // Yellow
+        'bg-yellow-100', 'bg-yellow-200', 'bg-yellow-300',
+        'bg-yellow-400', 'bg-yellow-500', 'bg-yellow-600',
+        'bg-yellow-700', 'bg-yellow-800', 'bg-yellow-900',
 
+        // Orange
+        'bg-orange-100', 'bg-orange-200', 'bg-orange-300',
+        'bg-orange-400', 'bg-orange-500', 'bg-orange-600',
+        'bg-orange-700', 'bg-orange-800', 'bg-orange-900',
+
+        // Amber
+        'bg-amber-100', 'bg-amber-200', 'bg-amber-300',
+        'bg-amber-400', 'bg-amber-500', 'bg-amber-600',
+        'bg-amber-700', 'bg-amber-800', 'bg-amber-900',
+
+        // Lime
+        'bg-lime-100', 'bg-lime-200', 'bg-lime-300',
+        'bg-lime-400', 'bg-lime-500', 'bg-lime-600',
+        'bg-lime-700', 'bg-lime-800', 'bg-lime-900',
+    ],
     theme: {
         extend: {
             fontFamily: {
@@ -18,6 +38,6 @@ export default {
             },
         },
     },
-
     plugins: [forms, typography],
 };
+
